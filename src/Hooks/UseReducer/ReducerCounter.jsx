5eab050c02482d3d92d0329d74aa5e3f105c2b5a
deshmukhketan647@gmail.com <59758054+ketan1234567@users.noreply.github.com>
2026-04-24 17:55:08 +0530

    "UseReducer"

--- src/Hooks/UseReducer/ReducerCounter.jsx
+++ src/Hooks/UseReducer/ReducerCounter.jsx
@@ -3,19 +3,35 @@ import React, { useReducer } from 'react'
 export const ReducerCounter = () => {
 
 
-    const reducer=(state,action)=>{};
+    const reducer=(state,action)=>{
+     /// console.log(state,action);
+      
+     if (action.type=="INCREMENT") {
+       return state+1
+     }
+         if (action.type=="DECREMENT") {
+       return state-1
+     }
+      if (action.type=="RESET") {
+       return state=0
+     }
+
+
+
+    };
 
     const [count,dispatch]=useReducer(reducer,0);
-    console.log(useReducer(reducer,0));
+    //console.log(useReducer(reducer,0));
     
 
 
   return (
     <>
-    <h1>Hello Reduer Hook:{}</h1>
+    <h1>Hello Reduer Hook:{count}</h1>
 
-    <button>Increment</button>
-      <button>Decrement</button>
+    <button onClick={()=>dispatch({type:"INCREMENT"})}>Increment</button>
+      <button onClick={()=>dispatch({type:"DECREMENT"})} >Decrement</button>
+      <button onClick={()=>dispatch({type:"RESET"})} >Reset</button>
    </>
   )
 }
